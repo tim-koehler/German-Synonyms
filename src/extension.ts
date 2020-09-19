34f537bb93bac5aa5f55dfc6cdf1cbaf65239a39
tim-koehler <timkoehler97@live.de>
2020-09-20 01:02:17 +0200

    ✨ first working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as https from 'https';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+interface Synonyms {
+	synsets: Synsets[]
+}
+
+interface Synsets {
+	terms: Term[]
+}
+
+interface Term {
+	term: string
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "German-Synonyms" is now active!');
+	let disposable = vscode.commands.registerCommand('German-Synonyms.lookupSynonym', () => {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('German-Synonyms.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
+		const text = editor?.document.getText(editor.selection);	
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from German Synonyms!');
+		https.get(`https://www.openthesaurus.de/synonyme/search?q=${text}&format=application/json`, (resp) => {
+            let data = '';
+
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+  
+            resp.on('end', () => {
+				const terms = getTerms(data);
+				console.log(terms);
+				
+				if (terms.length === 0) {
+					vscode.window.showInformationMessage(`No synonym found for '${text}'`);
+				} else {
+					showQuickpick(terms);
+				}
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
 	});
 
 	context.subscriptions.push(disposable);
@@ -25,3 +48,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function getTerms(data: any) {
+	const synonym: Synonyms = JSON.parse(data);
+
+	let synonymSet = new Set<string>();
+	synonym.synsets.forEach(synset => {
+		synset.terms.forEach(term => {
+			synonymSet.add(term.term);
+		});
+	});
+	return Array.from(synonymSet.values());
+}
+
+function showQuickpick(terms: string[]) {
+	const editor = vscode.window.activeTextEditor;
+	const quickPick = vscode.window.createQuickPick();
+	quickPick.items = terms.map((term: any) => ({ label: term }));
+	quickPick.onDidChangeSelection(([item]) => {
+		if (item) {
+			editor?.edit(edit => edit.replace(editor.selection, item.label));
+			quickPick.dispose();
+		}
+	});
+	quickPick.onDidHide(() => quickPick.dispose());
+	quickPick.show();
+}
